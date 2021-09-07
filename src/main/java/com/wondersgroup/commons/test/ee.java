2363f92bf1c912c3861ba6491b39da7b4b992515
tangjunlin1020 <844010980@qq.com>
2021-09-07 15:54:46 +0800

    sdsdsd

---
+++ src/main/java/com/wondersgroup/commons/test/ee.java
@@ -0,0 +1,15 @@
+package com.wondersgroup.commons.test;
+
+/**
+ * TODO
+ *
+ * @author tangjunlin
+ * @date 2021/9/6 18:14
+ * @since
+ */
+public class ee {
+    public static void main(String[] args) {
+        System.out.printf("1");
+        System.out.printf("3");
+    }
+}
