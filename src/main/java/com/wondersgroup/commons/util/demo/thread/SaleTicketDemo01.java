d57f375ab7d5b67acce915e973e6178f4f34e23e
tanjunlin1020 <844010980@qq.com>
2022-01-25 00:20:33 +0800

    excel

--- src/main/java/com/wondersgroup/commons/util/demo/thread/SaleTicketDemo01.java
+++ src/main/java/com/wondersgroup/commons/util/demo/thread/SaleTicketDemo01.java
@@ -1,53 +1,50 @@
-package com.wondersgroup.commons.thread;
+package com.wondersgroup.commons.util.demo.thread;
 
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.*;
-class Ticket //资源类 = 实例变量+实例方法
-{
+
+//资源类 = 实例变量+实例方法
+class Ticket {
     private int number = 30;
-//     List list = new ArrayList();
+    //     List list = new ArrayList();
     Lock lock = new ReentrantLock();
 
-    public void sale()
-    {
+    public void sale() {
         lock.lock();
-        try
-        {
-            if(number > 0)
-            {
-                System.out.println(Thread.currentThread().getName()+"\t卖出第："+(number--)+"\t 还剩下： "+number);
-            }
-        }catch (Exception e){
+        try {
+
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
+
+        }
+        try {
+            if (number > 0) {
+                System.out.println(Thread.currentThread().getName() + "\t卖出第：" + (number--) + "\t 还剩下： " + number);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             lock.unlock();
         }
     }
-
-
 }
 
 /**
- * @auther zzyy
- *
  * 题目：三个售票员         卖出          30张票
  * 笔记：如何编写企业级的多线程代码
- *  固定的编程套路+模板是什么？
- *
- *  1 在高内聚低耦合的前提下，线程        操作      资源类
- *
- *    1.1 一言不合，先创建一个资源类
- *
+ * 固定的编程套路+模板是什么？
+ * <p>
+ * 1 在高内聚低耦合的前提下，线程        操作      资源类
+ * <p>
+ * 1.1 一言不合，先创建一个资源类
  */
-public class SaleTicketDemo01
-{
+public class SaleTicketDemo01 {
     public static void main(String[] args)//主线程，一切程序的入口
     {
         Ticket ticket = new Ticket();
-
-
 //
 //        @FunctionalInterface
 //        public interface Runnable {
@@ -55,25 +52,26 @@ public class SaleTicketDemo01
 //            public abstract void run();
 //        }
 
-        new Thread(() -> { for (int i = 1; i <=40; i++) ticket.sale(); },"A").start();
-        new Thread(() -> { for (int i = 1; i <=40; i++) ticket.sale(); },"B").start();
-        new Thread(() -> { for (int i = 1; i <=40; i++) ticket.sale(); },"C").start();
+        new Thread(() -> {
+            for (int i = 1; i <= 40; i++) ticket.sale();
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 1; i <= 40; i++) ticket.sale();
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 1; i <= 40; i++) ticket.sale();
+        }, "C").start();
 
 
-        new Thread(new Runnable()
-        {
+        new Thread(new Runnable() {
             @Override
-            public void run()
-            {
-                for (int i = 1; i <=40; i++) ticket.sale();
+            public void run() {
+                for (int i = 1; i <= 40; i++) ticket.sale();
                 {
                     ticket.sale();
                 }
             }
         }, "A").start();
-
-
-
 
 
         //Thread(Runnable target, String name) Allocates a new Thread object.
@@ -99,18 +97,14 @@ public class SaleTicketDemo01
                 }
             }
         }, "B").start();*/
-        new Thread(new Runnable()
-        {
+        new Thread(new Runnable() {
             @Override
-            public void run()
-            {
-                for (int i = 1; i <=40; i++)
-                {
+            public void run() {
+                for (int i = 1; i <= 40; i++) {
                     ticket.sale();
                 }
             }
         }, "C").start();
-
 
 
     }
